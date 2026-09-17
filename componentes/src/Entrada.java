a1d877ca413240db0725d9c56035d9eeebc2e0ea
ericgonzalezg <eric.gonzalezg@cesjuanpablosegundo.es>
2026-09-17 12:33:11 +0200

    componentes

--- componentes/src/Entrada.java
+++ componentes/src/Entrada.java
@@ -5,6 +5,17 @@ public class Entrada {
         String apellido1 = "González";
         String apellido2 = "Galán";
         String correo = "eric.gonzalezg@cesjuanpablosegundo.es";
+        final String DNI = "123T";
+             int edad = -18;
+             edad = edad +2;
+             double alto = 1.85;
+             float altoFloat = 1.85f;
+             char letraDni = 'T';
+             boolean experiencia = true;
+             Object cosa = "Eric";
+
+
+
         System.out.println("Hola me llamo "+nombre+" "+apellido1+" "+apellido2);
         System.out.println("Mi correo es "+correo);
         System.out.printf("Mi nombre es %s y mis apellidos son %s %s y tengo %d años",
@@ -12,8 +23,26 @@ public class Entrada {
         }
 }
 
+
+                            //APUNTES//
 // "Mi nombre es %s y mis apellidos son %s %s y tengo %d años"
     //%s palabra
     //%d nº sin decimal
     //.2f nº con 2 decimales
     // debe de ir con System.out.printf
+
+//String-palabra " "
+//char-letra 'a'
+//int-numero sin coma     short/byte/long
+//double-numero con coma 1,78
+//float-numero con coma pero arrastra más numeros 1,78787087f (f final para float
+//boolean-true/false
+// Cuando la variable se encuentra en mayusculas (String) variable compleja
+    // si la variable esta en minusculas (int) es una variable simple
+//object se iguala a to_do  pero no se puede hacer tantas cosas como string
+// cuando delante del dato pones final se convierte en una cariable final
+        // y se pone en mayusculas, son constantes
+
+//TODO definir variables hacer algun mensaje...
+
+
