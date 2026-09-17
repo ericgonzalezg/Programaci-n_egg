6e076f3a3dde58bdc16f689e0a33a8ebaba4dccc
ericgonzalezg <eric.gonzalezg@cesjuanpablosegundo.es>
2026-09-17 11:58:55 +0200

    Update Entrada.java

--- componentes/src/Entrada.java
+++ componentes/src/Entrada.java
@@ -7,5 +7,13 @@ public class Entrada {
         String correo = "eric.gonzalezg@cesjuanpablosegundo.es";
         System.out.println("Hola me llamo "+nombre+" "+apellido1+" "+apellido2);
         System.out.println("Mi correo es "+correo);
+        System.out.printf("Mi nombre es %s y mis apellidos son %s %s y tengo %d años",
+                nombre,apellido1,apellido2,18);
         }
 }
+
+// "Mi nombre es %s y mis apellidos son %s %s y tengo %d años"
+    //%s palabra
+    //%d nº sin decimal
+    //.2f nº con 2 decimales
+    // debe de ir con System.out.printf
