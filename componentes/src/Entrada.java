21d8f6b42517a458ae986e3960480720aaf19910
ericgonzalezg <eric.gonzalezg@cesjuanpablosegundo.es>
2026-09-17 11:20:31 +0200

    Update Entrada.java

--- componentes/src/Entrada.java
+++ componentes/src/Entrada.java
@@ -2,10 +2,10 @@ public class Entrada {
 
         public static void main(String[] args){
         String nombre = "Eric";
-        String apellido1 = " González";
-        String apellido2 = " Galán";
+        String apellido1 = "González";
+        String apellido2 = "Galán";
         String correo = "eric.gonzalezg@cesjuanpablosegundo.es";
-        System.out.println("Hola "+"me llamo "+nombre + apellido1 + apellido2);
-        System.out.println("Mi "+ "correo "+"es "+correo);
+        System.out.println("Hola me llamo "+nombre+" "+apellido1+" "+apellido2);
+        System.out.println("Mi correo es "+correo);
         }
 }
