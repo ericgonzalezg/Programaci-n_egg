62866d311ee1e5ee20ade30e5a3a67a62e2e6707
ericgonzalezg <eric.gonzalezg@cesjuanpablosegundo.es>
2026-09-17 11:11:00 +0200

    update

---
+++ componentes/src/Entrada.java
@@ -0,0 +1,11 @@
+public class Entrada {
+
+        public static void main(String[] args){
+        String nombre = "Eric";
+        String apellido1 = " González";
+        String apellido2 = " Galán";
+        String correo = "eric.gonzalezg@cesjuanpablosegundo.es";
+        System.out.println("Hola "+"me llamo "+nombre + apellido1 + apellido2);
+        System.out.println("Mi "+ "correo "+"es "+correo);
+        }
+}
