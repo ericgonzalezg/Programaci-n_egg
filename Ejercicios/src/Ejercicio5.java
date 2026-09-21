3ef0c65f19ddb7ca0f1f881a7cdf2d18b78aec13
ericgonzalezg <eric.gonzalezg@cesjuanpablosegundo.es>
2026-09-21 13:03:17 +0200

    Create Ejercicio5.java

---
+++ Ejercicios/src/Ejercicio5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    /* Hágase un programa que convierta segundos en horas, minutos y segundos.(Segundos)
+
+*ENTRADA/SALIDA*
+
+Número de segundos: **24973**
+
+Horas: 6
+
+Minutos: 56
+
+Segundos: 13 */
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Hola buenas me puede indicar el número de segundos");
+        int segundostot = lector.nextInt();
+        int segundos = segundostot%60;
+        int minutos = segundostot/60;
+        int horas = minutos/60;
+        System.out.println("Horas: "+horas);
+        System.out.println("Minutos: "+minutos);
+        System.out.println("Segundos: "+segundos);
+
+    }
+}
